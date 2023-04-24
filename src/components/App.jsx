db99e62e7bffc23ccbeccd068abb9ab94af2807c
MykhayloOdesa <m.yervachov@gmail.com>
2023-04-24 14:20:43 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+}
